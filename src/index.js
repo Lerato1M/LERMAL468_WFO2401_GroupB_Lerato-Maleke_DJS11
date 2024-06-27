d2d6be9c6785636c7b1e4039e4ea815e68f5545d
Lerato Maleke <leratomaleke.m@gmail.com>
2024-06-27 13:17:19 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Import global styles
-import App from './App'; // Import the main App component
+import App from './Components/App'; // Import the main App component
 import reportWebVitals from './reportWebVitals'; // Import the function to measure web vitals
 
 // Render the App component inside the root element of the HTML document
